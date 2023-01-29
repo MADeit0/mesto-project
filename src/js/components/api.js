6bf324720feac32597232ae9bb349acff969b142
pavel putilin <starikan123@gmail.com>
2023-01-29 23:01:49 +0300

    feat: added api class

--- src/js/components/api.js
+++ src/js/components/api.js
@@ -1,97 +1,86 @@
-import { cohortId, token } from '../utils/constants';
+import { cohortId, token } from "../utils/constants";
 
-const config = {
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _isResponse(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка ${res.status}`);
+    }
+  }
+
+  isRejected(err) {
+    console.error(err);
+  }
+
+  getInitialProfile() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => this._isResponse(res));
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then((res) => this._isResponse(res));
+  }
+
+  setProfileData(profileName, profileAbout) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: profileName,
+        about: profileAbout,
+      }),
+    }).then((res) => this._isResponse(res));
+  }
+
+  setNewCard(cardName, cardLink) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: cardName,
+        link: cardLink,
+      }),
+    }).then((res) => this._isResponse(res));
+  }
+
+  setProfileAvatar(link) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: link,
+      }),
+    }).then((res) => this._isResponse(res));
+  }
+
+  cardDelete(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => this._isResponse(res));
+  }
+
+  putLikeCard(cardId, methodToggle) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: methodToggle,
+      headers: this._headers,
+    }).then((res) => this._isResponse(res));
+  }
+}
+
+const api = new Api({
   baseUrl: `https://nomoreparties.co/v1/${cohortId}`,
   headers: {
     authorization: token,
-    'Content-Type': 'application/json'
-  }
-}
-
-const isResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-  } else {
-    return Promise.reject(`Ошибка ${res.status}`);
-  }
-}
-
-const isRejected = (err) => {
-  console.log(err);
-}
-
-const getInitialProfile = () => {
-  return fetch(`${config.baseUrl}/users/me`, {
-    headers: config.headers
-  })
-    .then(res => isResponse(res));
-}
-
-const getInitialCards = () => {
-  return fetch(`${config.baseUrl}/cards`, {
-    headers: config.headers
-  })
-    .then(res => isResponse(res));
-}
-
-const setProfileData = (profileName, profileAbout) => {
-  return fetch(`${config.baseUrl}/users/me`, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: profileName,
-      about: profileAbout
-    })
-  })
-    .then(res => isResponse(res));
-}
-
-const setNewCard = (cardName, cardLink) => {
-  return fetch(`${config.baseUrl}/cards`, {
-    method: 'POST',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: cardName,
-      link: cardLink
-    })
-  })
-    .then(res => isResponse(res));
-}
-
-const setProfileAvatar = (link) => {
-  return fetch(`${config.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      avatar: link
-    })
-  })
-    .then(res => isResponse(res));
-}
-
-const cardDelete = (cardId) => {
-  return fetch(`${config.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-    .then(res => isResponse(res));
-}
-
-const putLikeCard = (cardId, methodToggle) => {
-  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: methodToggle,
-    headers: config.headers
-  })
-    .then(res => isResponse(res));
-}
-
-export {
-  getInitialProfile,
-  getInitialCards,
-  isRejected,
-  setProfileData,
-  setNewCard,
-  setProfileAvatar,
-  putLikeCard,
-  cardDelete
-}
+    "Content-Type": "application/json",
+  },
+});
